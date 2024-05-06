af8bf976bb624822da8509a1e314b785cbee881d
Sisamnkelo Hlati <hlatisisamnkelo@gmail.com>
2024-05-06 21:33:40 +0200

    added padding to the bottom footer

--- src/components/BottomFooter.jsx
+++ src/components/BottomFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BottomFooter() {
   return (
     <div className='w-screen h-full border-t border-zinc-100  bg-[#FEFEFE]'>
-    <div className='md:max-w-[1240px] mx-auto px-5  h-full  '>
+    <div className='md:max-w-[1240px] mx-auto px-5 py-3 h-full  '>
       
       <div className=' flex items-center justify-between text-zinc-800 w-full flex-wrap '>
         <p className=' mb-3 text-sm'>Copyright &copy; 2024, all rights reserved - Images By Unsplash</p>
